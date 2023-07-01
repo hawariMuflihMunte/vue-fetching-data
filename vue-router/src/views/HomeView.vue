8bf5c264223580b6c62653b6158a2593ee3f00e3
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-07-01 12:21:27 +0700

    Upload existing vue project

---
+++ vue-router/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita?</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita?</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+  components: {}
+}
+</script>
