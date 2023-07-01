5531728fbe21df4cebfdf5462b25ad83f415a143
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-07-01 12:52:18 +0700

    Conditional render for job details

--- vue-router/src/views/jobs/JobDetails.vue
+++ vue-router/src/views/jobs/JobDetails.vue
@@ -1,11 +1,28 @@
 <template>
-    <h1>Job Details</h1>
-    <p>ID: {{ id }}</p>
+    <div v-if="job">
+      <h1>{{ job.title }}</h1>
+      <p>ID: {{ id }}</p>
+      <p>{{ job.details }}</p>
+    </div>
+    <div v-else>
+      <p>Loading...</p>
+    </div>
 </template>
 
 <script>
 export default {
   props: ['id'],
+  data() {
+    return {
+      job: null
+    }
+  },
+  mounted() {
+    fetch('http://localhost:3000/jobs/' + this.id)
+      .then(response => response.json())
+      .then(data => this.job = data)
+      .catch(error => console.log(error.message))
+  }
 }
 </script>
 
